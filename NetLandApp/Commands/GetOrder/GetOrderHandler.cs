61cb245f84229489e4f3dd6d6eee14f5bc84d8c4
Piotr Wisniewski <piowisni@gmail.com>
2023-04-28 18:52:41 +0200

    Added query and handler

---
+++ NetLandApp/Commands/GetOrder/GetOrderHandler.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using NetLandApp.Models;
+using NetLandApp.NetLandApp.Queries;
+using NetLandApp.Services;
+
+namespace NetLandApp.NetLandApp.Commands.GetOrder
+{
+    public class GetOrderHandler : IRequestHandler<GetOrderQuery, IEnumerable<Order>>
+    {
+        private readonly CsvService _csvService;
+        private readonly IConfiguration _configuration;
+
+        public GetOrderHandler(CsvService csvService, IConfiguration configuration)
+        {
+            _csvService = csvService;
+            _configuration = configuration;
+        }
+
+        public Task<IEnumerable<Order>> Handle(GetOrderQuery request, CancellationToken cancellationToken)
+        {
+            string filePath = _configuration.GetValue<string>("OrdersCsvPath");
+            var orders = _csvService.Read(filePath);
+
+            if (!string.IsNullOrEmpty(request.Number))
+            {
+                orders = orders.Where(o => o.Number == request.Number);
+            }
+
+            if (request.OrderDateFrom.HasValue && request.OrderDateTo.HasValue)
+            {
+                orders = orders.Where(o => o.OrderDate >= request.OrderDateFrom.Value && o.OrderDate <= request.OrderDateTo.Value);
+            }
+
+            if (request.ClientCodes != null && request.ClientCodes.Any())
+            {
+                orders = orders.Where(o => request.ClientCodes.Contains(o.ClientCode));
+            }
+
+            return Task.FromResult(orders);
+        }
+    }
+
+}
---
+++ NetLandApp/Queries/GetOrderQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using NetLandApp.Models;
+
+namespace NetLandApp.NetLandApp.Queries
+{
+    public class GetOrderQuery : IRequest<IEnumerable<Order>>
+    {
+        public string Number { get; set; }
+        public DateTime? OrderDateFrom { get; set; }
+        public DateTime? OrderDateTo { get; set; }
+        public List<string> ClientCodes { get; set; }
+    }
+}
