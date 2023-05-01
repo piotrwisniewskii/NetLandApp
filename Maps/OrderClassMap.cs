a6ccb16c6b2ac3ee41fa9509c29b043f68e9d855
Piotr Wisniewski <piowisni@gmail.com>
2023-05-01 09:26:29 +0200

    maping added

--- Maps/OrderClassMap.cs
+++ Maps/OrderClassMap.cs
@@ -7,13 +7,13 @@ public class OrderClassMap : ClassMap<Order>
     public OrderClassMap()
     {
         AutoMap(CultureInfo.InvariantCulture);
-        Map(m => m.Number).Name("Number");
-        Map(m => m.ClientCode).Name("ClientCode");
-        Map(m => m.ClientName).Name("ClientName");
-        Map(m => m.OrderDate).Name("OrderDate");
-        Map(m => m.ShipmentDate).Name("ShipmentDate");
-        Map(m => m.Quantity).Name("Quantity");
-        Map(m => m.Confirmed).Name("Confirmed");
-        Map(m => m.Value).Name("Value");
+        Map(m => m.Number).Index(0);
+        Map(m => m.ClientCode).Index(1);
+        Map(m => m.ClientName).Index(2);
+        Map(m => m.OrderDate).Index(3);
+        Map(m => m.ShipmentDate).Index(4);
+        Map(m => m.Quantity).Index(5);
+        Map(m => m.Confirmed).Index(6);
+        Map(m => m.Value).Index(7);
     }
 }
--- Services/CsvService.cs
+++ Services/CsvService.cs
@@ -10,8 +10,12 @@ namespace NetLandApp.Services
     {
         public IEnumerable<Order> Read(string filePath)
         {
+            var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+            };
             using var reader = new StreamReader(filePath);
-            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            using var csv = new CsvReader(reader, configuration);
             csv.Context.RegisterClassMap<OrderClassMap>();
             var records = csv.GetRecords<Order>().ToList();
             return records;
